2a03415998607e065e5954c279ac55df35087cab
Denis Kibet <kibet1816@gmail.com>
2019-06-23 21:28:32 +0300

    Add two routes

--- app/views.py
+++ app/views.py
@@ -9,3 +9,11 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
