8f7696875dd6bcf089b7d591663562ec26130753
Denis Kibet <kibet1816@gmail.com>
2019-06-23 21:37:58 +0300

    Add title

--- app/views.py
+++ app/views.py
@@ -8,7 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - Read all news here'
+    return render_template('index.html', title = title)
 
 @app.route('/news/<news_id>')
 def news(news_id):
