29c4f50ccee7fd6346001e91cf4d5f30a3b859a8
Denis Kibet <kibet1816@gmail.com>
2019-06-23 23:26:57 +0300

    Make first API call

--- app/requests.py
+++ app/requests.py
@@ -9,4 +9,34 @@ api_key = app.config['NEWS_API_KEY']
 base_url = app.config['NEWS_API_BASE_URL']
 
 def get_source(id):
-    pass
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_source_url = base_url.format(id,api_key)
+    with urllib.request.urlopen(get_source_url) as url:
+        get_source_data = url.read()
+        get_source_response = json.loads(get_source_data)
+
+        source_results = None
+
+        if get_source_response['results']:
+            source_results_list = get_source_response['results']
+            source_results = process_results(source_results_list)
+            return source_results
+
+
+def process_results(source_list):
+    """
+    Function  that processes the movie result and transform them to a list of Objects
+    """
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        poster = source_item.get('urlToImage')
+    
+        if poster:
+            source_object = Source(id,name,poster)
+            source_results.append(source_object)
+
+    return source_results
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .requests import get_source
 
 # Views
 @app.route('/')
@@ -8,8 +9,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    news_source = get_source('id')
+    print(news_source)
     title = 'Home - Read all news here'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,news = news_source)
 
 @app.route('/news/<news_id>')
 def news(news_id):
